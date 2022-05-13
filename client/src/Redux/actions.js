699e47b430ee4323a8f42ecbe136a3e7edec89e7
EmiCon97 <derleuchtturm97@gmail.com>
2022-05-13 13:32:07 -0300

    delete

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -73,6 +73,7 @@ export function postFakemon (payload) {
    };
 };
 export function deleteFakemon (id) {
+   JSON.stringify(id);
    return async dispatch => {
       return await axios.delete(`https://pokemonshowroom.herokuapp.com/pokemons${id}`)
       //return await axios.delete(`http://localhost:3001/pokemons/${payload}`)
